c731195f11a71b60a2688fad413d6e19154815f4
Leo Cortez <leonardocortez389@gmail.com>
2022-04-12 15:45:25 -0500

    Agregué un println en el archivo Prueba1

--- src/main/java/Prueba1/Prueba1.java
+++ src/main/java/Prueba1/Prueba1.java
@@ -4,5 +4,6 @@ import java.util.Scanner;
 public class Prueba1{
     public static void main(String args[]){
      System.out.println("Hola, haz algún cambio");
+     System.out.println("Este es otra salida");
     }
 }
